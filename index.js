01d91a584f2cb8078bd290638cd6f3ecaddb89b8
safi <7saffix@gmail.com>
2024-10-06 21:22:27 +0600

    database connection added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
+
+//CONNECT DB
+(async()=>{
+    try {
+       await mongoose.connect('mongodb://localhost:27017/studentRegistration',{ autoIndex: true }) ;
+      console.log('mongoDB connected successfully');
+    } catch (error) {
+        console.log(error)
+    }
+})();
 
 app.listen(()=>{
     console.log(`server running success at port :: ${PORT}`);
