f3266d1a7d6c3573bbbf91047294073924776f3f
safi <7saffix@gmail.com>
2024-10-08 12:30:21 +0600

    created file upload and read using multer

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,15 +1,19 @@
 import express from "express";
 import { readProfile, studentLogin, studentRegistration, updateProfile } from "../controllers/userController.js";
 import { authVerify } from "../middlewares/authMiddleware.js";
+import { readFile, uploadFile } from "../controllers/fileController.js";
 
 const router =express.Router();
 
 
 //student api
-router.post('/studentRegistration',studentRegistration)
-router.post('/studentLogin',studentLogin)
-router.get('/readProfile',authVerify,readProfile)
-router.post('/updateProfile',authVerify,updateProfile)
+router.post('/studentRegistration',studentRegistration);
+router.post('/studentLogin',studentLogin);
+router.get('/readProfile',authVerify,readProfile);
+router.post('/updateProfile',authVerify,updateProfile);
 
+//file api
+router.post('/upload',uploadFile);
+router.get('/read/:filename', readFile); 
 
 export default router
